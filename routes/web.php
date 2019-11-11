6d33e1aa6fba5adc1492f193618bf69e437c98d2
Mashal731 <56961969+Mashal731@users.noreply.github.com>
2019-11-11 16:02:33 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/signin', function () {
+    return view('signin');
+});
+Route::get('/layout', function () {
+    return view('layout');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/home', function () {
+    return view('welcome');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
